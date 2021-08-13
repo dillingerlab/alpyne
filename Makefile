07301adb78c2d23c26063f3b20223d6d259ce4e8
Austin D <atdillinger92@gmail.com>
2021-08-12 19:53:24 -0500

    working api; now to inspect and decide on status

---
+++ Makefile
@@ -0,0 +1,22 @@
+SHELL := /bin/bash
+
+.DEFAULT_GOAL := help
+VENV=$(CURDIR)/.venv/bin/activate
+
+$(VENV)::$(CURDIR)/requirements.txt
+	python3 -m venv $(CURDIR)/.venv
+	chmod 700 $(CURDIR)/.venv/bin/activate
+	( \
+	   source $(CURDIR)/.venv/bin/activate; \
+	   pip install wheel; \
+	   pip install -r $(CURDIR)/requirements.txt; \
+	)
+
+
+dev:
+	$(MAKE) $(VENV)
+
+
+.PHONY: help
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from datetime import datetime
+from pprint import pprint
+
+import requests
+
+
+def main():
+    """Description of the function"""
+    api_key = os.environ['openweather_api_key']
+    # Winona Latitude: 44.07468410 Longitude: -91.67587140
+    lat_lon = '44.07468410&lon=-91.67587140'
+    excluded_dataset = 'current,minutely,hourly,alerts '
+    url = 'https://api.openweathermap.org'
+    uri = f'{url}/data/2.5/onecall?lat={lat_lon}&exclude={excluded_dataset}&units=imperial&appid={api_key}'
+    r = requests.get(uri)
+    data = r.json()
+    timezone = data['timezone']
+    for x in data['daily']:
+        date = datetime.fromtimestamp(x['dt'])
+        pprint('date: ' + str(date) + timezone)
+        pprint('feels like: ' + str(x['feels_like']))
+        pprint('temps: ' + str(x['temp']))
+        pprint('rain: ' + str(x.get('rain')))
+        pprint('humidity: ' + str(x['humidity']))
+
+
+if __name__ == '__main__':
+    main()
