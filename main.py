1f39d1497c3efb6e870fac3cfe069403ec89a50a
Austin D <atdillinger92@gmail.com>
2021-10-27 18:31:22 -0500

    remember what I forgot; var name expansion, use pprint

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def get_working_dataset(latitude_longitude):
         dataset[days] = {}
         dataset[days]['date'] = datetime.fromtimestamp(x['dt'])
         dataset[days]['timezone'] = data['timezone']
-        dataset[days]['day_fl_temp'] = x['feels_like']['day']
-        dataset[days]['night_fl_temp'] = x['feels_like']['night']
+        dataset[days]['day_feels_like_temp'] = x['feels_like']['day']
+        dataset[days]['night_feels_like_temp'] = x['feels_like']['night']
         dataset[days]['weather'] = x['weather'][0]['description']
         days += 1
     return dataset
@@ -30,7 +30,7 @@ def main():
     # Winona Latitude: 44.07468410 Longitude: -91.67587140
     lat_lon = '44.07468410&lon=-91.67587140'
     dataset = get_working_dataset(lat_lon)
-    print(dataset)
+    pprint(dataset[0])
 
 
 if __name__ == '__main__':
