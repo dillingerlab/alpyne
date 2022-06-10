03f89c4bcfe4ed9b19ea991a5e75db9a043b43db
Austin <shuffletick@gmail.com>
2022-06-09 18:31:45 -0600

    init rating method

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ import requests
 import yaml
 
 
+def calc_rating(temp_max, temp_low, day_feel_like, night_feel_like):
+    rating = 0
+    high = {-1: list(range(-30, 40)),
+            -2: list(range(40, 60)),
+            3: list(range(60, 80)),
+            2: list(range(80, 90)),
+            1: list(range(90, 130))}
+    for ranger in high:
+        if temp_max in high[ranger]:
+            rating = ranger
+    return rating
+
+
 def get_working_dataset(latitude, longitude):
     api_key = os.environ["openweather_api_key"]
     excluded_dataset = "current,minutely,hourly,alerts"
@@ -27,6 +40,9 @@ def get_working_dataset(latitude, longitude):
         dataset[days]["temp_max"] = x["temp"]["max"]
         dataset[days]["night_feels_like_temp"] = x["feels_like"]["night"]
         dataset[days]["weather"] = x["weather"][0]["description"]
+
+        dataset[days]["rating"] = calc_rating(1, 2, 3, 4)
+
         days += 1
     return dataset
 
@@ -44,8 +60,10 @@ def main():
         longitude = doc[i]["longitude"]
 
         dataset = get_working_dataset(latitude, longitude)
-        print(f"Crag: {i}")
-        pprint(dataset)
+    print(f"Crag: {i}")
+    #for day in dataset:
+    #    pprint(dataset[day])
+    pprint(calc_rating(76.5, 2, 3, 4))
     # Call Twilio
 
 
