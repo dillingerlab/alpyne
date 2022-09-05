79fdbec5939984f1e54e84f1830a9f1746cfd792
Austin <shuffletick@gmail.com>
2022-09-04 18:05:12 -0600

    reutrn sms compatible payload

--- lambdas/alpyne/main.py
+++ lambdas/alpyne/main.py
@@ -1,16 +1,13 @@
 #!./.venv/bin/python3
-
+import json
 import os
 from datetime import datetime
+import logging
 from pprint import pprint
-
+import boto3
 import requests
 import yaml
-
-import boto3
-import base64
-import json
-from botocore.exceptions import ClientError
+from textwrap import dedent
 
 
 def calc_rating(category: str, temperature: float):
@@ -24,17 +21,44 @@ def calc_rating(category: str, temperature: float):
     return rating
 
 
-def get_working_dataset(latitude, longitude):
-    api_key = get_secret(secret_container='openweather',
-        region_name='us-east-1',
-        secret_key='api_secret')
+def get_secret(secret_container: str, region_name: str, secret_key: str) -> str:
+
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+    get_secret_value_response = client.get_secret_value(SecretId=secret_container)
+    secrets = json.loads(get_secret_value_response["SecretString"], strict=False)
+    return secrets[secret_key]
+
+
+def get_working_dataset(latitude: float, longitude: float) -> dict:
+    """
+    Connect to SecretsManager and get API secret for OpenWeather,
+    then get and return weather data for lat/lon
+
+    TODO: Function is do this and this and this, should be purposeful
+    """
+    if os.environ["openweather_api_key"]:
+        logging.info('using OS')
+        api_key = os.environ["openweather_api_key"]
+    else: 
+        logging.info('using secretsmanager')
+        api_key = get_secret(secret_container="openweather",
+            region_name="us-east-1",
+            secret_key="api_secret")
 
     excluded_dataset = "current,minutely,hourly,alerts"
     url = "https://api.openweathermap.org"
-    uri = f"data/2.5/onecall?lat={latitude}&lon={longitude}&exclude={excluded_dataset}&units=imperial&appid={api_key['api_secret']}"
+    uri = f"data/2.5/onecall?lat={latitude}&lon={longitude}&exclude={excluded_dataset}&units=imperial&appid={api_key}"
     api_endpoint = f"{url}/{uri}"
     r = requests.get(api_endpoint)
-    data = r.json()
+    return r.json()
+
+
+def update_weather_data(data: dict) -> dict:
+    """
+    Updates OpenWeather Dataset as needed
+    """
     dataset = {}
     days = 0
     for x in data["daily"]:
@@ -53,24 +77,31 @@ def get_working_dataset(latitude, longitude):
     return dataset
 
 
+def get_weekend_status(data: dict) -> dict:
+    """
+    Updates OpenWeather Dataset as needed
+    """
+    dataset = {}
+    days = 0
+    for x in data["daily"]:
+        # pprint(x)
+        dataset[days] = {}
+        dataset[days]["date"] = datetime.fromtimestamp(x["dt"]).strftime("%m/%d")
+        dataset[days]["day_of_the_week"] = datetime.fromtimestamp(x["dt"]).strftime("%A")
+        dataset[days]["high"] = x["temp"]["max"]
+        dataset[days]["weather"] = x["weather"][0]["description"]
 
-def get_secret(secret_container: str, region_name: str, secret_key: str) -> str:
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    get_secret_value_response = client.get_secret_value(
-        SecretId=secret_container
-    )
-    return json.loads(get_secret_value_response['SecretString'], strict=False)
+        days += 1
+    return dataset
 
 
-def main():
-    # Winona Latitude: 44.07468410 Longitude: -91.67587140
+def weekend_forecast():
+    """
+    refactor to a generator to send data?
+    """
     with open("cords.yml", "r") as f:
         doc = yaml.load(f, Loader=yaml.FullLoader)
+    
     keys = []
     for x in doc:
         keys.append(x)
@@ -79,15 +110,20 @@ def main():
         latitude = doc[i]["latitude"]
         longitude = doc[i]["longitude"]
 
-        dataset = get_working_dataset(latitude, longitude)
-    print(f"Crag: {i}")
-    pprint(f"Dataset: {dataset[0]}")
+        raw_data = get_working_dataset(latitude, longitude)
+        dataset = get_weekend_status(raw_data)
 
-    # Call Twilio
+    output = dedent(f"""
+        Crag: {i}
+        Day: {dataset[0]['day_of_the_week']}, {dataset[0]['weather']}, {dataset[0]['high']}
+        Day: {dataset[1]['day_of_the_week']}, {dataset[1]['weather']}, {dataset[1]['high']}
+        Day: {dataset[2]['day_of_the_week']}, {dataset[2]['weather']}, {dataset[2]['high']}
+        """)
+    return output
 
 
 def handler(event, context):
-    main()
+    weekend_forecast()
     # secretManagerClient(<region>)
     # GetSecretValueCommand(<secretid>)
     # response = clitn)
@@ -95,4 +131,4 @@ def handler(event, context):
 
 
 if __name__ == "__main__":
-    main()
+    print(weekend_forecast())
